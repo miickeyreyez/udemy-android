6d9a3e27a984b210bcc31983e452baedcc84d4d5
Ángel Reyes <maxr@praxis.com.mx>
2018-04-18 10:18:00 -0500

    Downgrade de gradle version a 4.1 de 4.4 y del plugin de 3.1.0 a 3.0.0

--- HamburguerMenu/app/src/main/java/udemy/com/hamburguermenu/MainActivity.java
+++ HamburguerMenu/app/src/main/java/udemy/com/hamburguermenu/MainActivity.java
@@ -1,15 +1,13 @@
 package udemy.com.hamburguermenu;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.FocusFinder;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
