122096bf458441a3a03c127524ac7827b215f8c6
Angel Reyes <miickeyreyez@gmail.com>
2018-04-21 23:03:43 -0500

    Sección 11:Se implementa que se guarde el estado del switch del dialog, se agrega APK

--- HamburguerMenuApplication/app/src/main/java/com/udemy/hmenu/hamburguermenuapplication/AlertFragment.java
+++ HamburguerMenuApplication/app/src/main/java/com/udemy/hmenu/hamburguermenuapplication/AlertFragment.java
@@ -40,6 +40,38 @@ public class AlertFragment extends Fragment {
         textView = view.findViewById(R.id.textViewDialogAlerts);
         flag = false;
 
+        //Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater layoutInflater = getLayoutInflater();
+        final View dialog = layoutInflater.inflate(R.layout.alert_dialog_alerts,null);
+
+        builder.setView(dialog);
+        final AlertDialog alertDialog = builder.create();
+
+        TextView textViewDialog = (TextView)dialog.findViewById(R.id.textViewCloseDialog);
+        switchAlert = (Switch)dialog.findViewById(R.id.switchAlerts);
+
+        switchAlert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(switchAlert.isChecked()) {
+                    textView.setText("Alerts enabled");
+                    switchAlert.setChecked(true);
+                } else {
+                    textView.setText("Alerts disabled");
+                    switchAlert.setChecked(false);
+                }
+            }
+        });
+
+        textViewDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
         //fab.setOnClickListener(this);
         //El override se hace:
         //@Override
@@ -47,40 +79,7 @@ public class AlertFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                // 1. Instantiate an AlertDialog.Builder with its constructor
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                LayoutInflater layoutInflater = getLayoutInflater();
-                final View dialog = layoutInflater.inflate(R.layout.alert_dialog_alerts,null);
-
-                builder.setView(dialog);
-                final AlertDialog alertDialog = builder.create();
-
-                TextView textViewDialog = (TextView)dialog.findViewById(R.id.textViewCloseDialog);
-                switchAlert = (Switch)dialog.findViewById(R.id.switchAlerts);
-
-                switchAlert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                        if(switchAlert.isChecked()) {
-                            textView.setText("Alerts enabled");
-                            switchAlert.setChecked(true);
-                        } else {
-                            textView.setText("Alerts disabled");
-                            switchAlert.setChecked(false);
-                        }
-                    }
-                });
-
-                textViewDialog.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        alertDialog.dismiss();
-                    }
-                });
-
                 alertDialog.show();
-
             }
         });
 
