334bbef92a2798d906dfa13ba6406a7eb2e2391c
Angel Reyes <miickeyreyez@gmail.com>
2018-04-23 22:07:53 -0500

    Sección 10: Creando toolbar

---
+++ Toolbar/app/src/main/java/com/udemy/toolbar/toolbar/MainActivity.java
@@ -0,0 +1,24 @@
+package com.udemy.toolbar.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
