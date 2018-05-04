3e2267ec70064d85c01d7bec40d9d41ebbbbedee
Angel Reyes <miickeyreyez@gmail.com>
2018-05-03 23:06:30 -0500

    Sección 12: Avance de markers personalizados, cambiar el title y el snippet cuando haces un drag

--- Maps/app/src/main/java/com/udemy/maps/MapFragment.java
+++ Maps/app/src/main/java/com/udemy/maps/MapFragment.java
@@ -10,17 +10,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -32,8 +37,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     private View view;
     private GoogleMap googleMap;
     private MapView mapView;
-    private Address address;
+    private List<Address> address;
     private Geocoder geocoder;
+    private MarkerOptions markerOptions;
 
     public MapFragment() {
         // Required empty public constructor
@@ -71,14 +77,23 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         //Tilt es para 3D, límite 90
         LatLng azteca = new LatLng(19.302861  , -99.150528);
         LatLng campNou = new LatLng(41.380896 , 2.12282);
-        googleMap.addMarker(new MarkerOptions().position(campNou).title("Camp Nou").draggable(true));
-        googleMap.addMarker(new MarkerOptions().position(azteca).title("Azteca"));
+        //googleMap.addMarker(new MarkerOptions().position(campNou).title("Camp Nou").draggable(true));
+        //googleMap.addMarker(new MarkerOptions().position(azteca).title("Azteca"));
 
         CameraPosition camera = new CameraPosition.Builder().target(campNou)
                 .zoom(18)
                 .bearing(45)
                 .tilt(90)
                 .build();
+
+        markerOptions = new MarkerOptions();
+        markerOptions.position(campNou);
+        markerOptions.title("Camp Nou");
+        markerOptions.draggable(true);
+        markerOptions.snippet("Estadio FC Barcelona");
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.star_on));
+
+        googleMap.addMarker(markerOptions);
 
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 
@@ -89,6 +104,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         googleMap.setOnMarkerDragListener(this);
 
         geocoder = new Geocoder(getContext(), Locale.getDefault());
+
     }
 
     @Override
@@ -105,5 +121,23 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     public void onMarkerDragEnd(Marker marker) {
         double latitude = marker.getPosition().latitude;
         double longitude = marker.getPosition().longitude;
+
+        try {
+            address = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //AddressLine -> Solamente la primera linea
+        Address add = address.get(0);
+        String addressString = add.getAddressLine(0);
+        String city = add.getLocality();
+        String state = add.getAdminArea();
+        String country = add.getCountryName();
+        String zipCode = add.getPostalCode();
+        String direccion = city + " " + state + " " + country + " " + zipCode;
+        marker.setTitle(addressString);
+        marker.setSnippet(direccion);
+        //Toast.makeText(getContext(), direccion ,Toast.LENGTH_SHORT).show();
     }
 }
