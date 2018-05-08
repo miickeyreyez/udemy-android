ad7e15980c5b16fa3154d15e96ff80d70875280c
Angel Reyes <miickeyreyez@gmail.com>
2018-05-07 22:07:59 -0500

    Sección 13: Multilenguaje, implementación de XML inglés/español

---
+++ Multilenguaje/app/src/main/java/com/udemy/multilenguaje/Example.java
@@ -0,0 +1,13 @@
+package com.udemy.multilenguaje;
+
+import android.content.Context;
+
+/**
+ * Created by INSPIRON on 7/5/2018.
+ */
+
+public class Example {
+    public static String getWelcome(Context context) {
+        return context.getString(R.string.welcome);
+    }
+}
---
+++ Multilenguaje/app/src/main/java/com/udemy/multilenguaje/MainActivity.java
@@ -0,0 +1,15 @@
+package com.udemy.multilenguaje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,getString(R.string.welcome),Toast.LENGTH_SHORT).show();
+    }
+}
